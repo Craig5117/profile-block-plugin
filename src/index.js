e7ef8cd276de1f6905f0d0727b7d09f83c8fb35c
Craig5117 <craig.bennett5117@gmail.com>
2021-06-30 15:45:24 -0400

    formatted with prettier

--- src/index.js
+++ src/index.js
@@ -2,44 +2,66 @@ import { registerBlockType } from '@wordpress/blocks';
 import { InnerBlocks, useBlockProps } from '@wordpress/block-editor';
 
 registerBlockType('plugin/profile-block', {
-    title: 'Profile Block',
-    icon: 'id',
-    category: 'common',
-    // attributes: {
-    //   profileHeading: {type: 'string'},
-    //   profileImage: {type: 'string'},
-    // },
-    edit() {
-		const blockProps = useBlockProps();
-        const TEMPLATE = 
-        [  ['core/group', { className:"profile-container" }, [
-            ['core/heading', { className:"profile-header" }],
-			['core/group', { className:"profile-main" }, [
-                    [ 'core/image', { className: "profile-image"}],
-                 
-				['core/group', { className:"profile-details" }, [
-					[ 'core/list', {className: "profile-list", placeholder: 'You can list details here...'} ],
-                    [ 'core/paragraph', { className: "profile-info", placeholder: 'Enter some information here...' } ],
-                ] ],
-            ] 
-        ]
-    ]
-]];
-		return (
-			<div { ...blockProps }>
-				<InnerBlocks template={TEMPLATE}/>                
-			</div>
-		);
-	},
+  title: 'Profile Block',
+  icon: 'id',
+  category: 'common',
+  // attributes: {
+  //   profileHeading: {type: 'string'},
+  //   profileImage: {type: 'string'},
+  // },
+  edit() {
+    const blockProps = useBlockProps();
+    const TEMPLATE = [
+      [
+        'core/group',
+        { className: 'profile-container' },
+        [
+          ['core/heading', { className: 'profile-header' }],
+          [
+            'core/group',
+            { className: 'profile-main' },
+            [
+              ['core/image', { className: 'profile-image' }],
 
-	save() {
-		const blockProps = useBlockProps.save();
+              [
+                'core/group',
+                { className: 'profile-details' },
+                [
+                  [
+                    'core/list',
+                    {
+                      className: 'profile-list',
+                      placeholder: 'You can list details here...',
+                    },
+                  ],
+                  [
+                    'core/paragraph',
+                    {
+                      className: 'profile-info',
+                      placeholder: 'Enter some information here...',
+                    },
+                  ],
+                ],
+              ],
+            ],
+          ],
+        ],
+      ],
+    ];
+    return (
+      <div {...blockProps}>
+        <InnerBlocks template={TEMPLATE} />
+      </div>
+    );
+  },
 
-		return (
-			<div { ...blockProps }>
-				<InnerBlocks.Content />
-			</div>
-		);
-	},
-} );
-  
+  save() {
+    const blockProps = useBlockProps.save();
+
+    return (
+      <div {...blockProps}>
+        <InnerBlocks.Content />
+      </div>
+    );
+  },
+});
